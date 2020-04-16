059f51f21dc97be26ed5b770f6c562452add4b33
Oscar Ablinger <dev.ablinger@gmail.com>
2020-04-16 23:03:26 +0200

    feat(server): implement basics

---
+++ autosupport-lsp-server/AutosupportLanguageDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace autosupport_lsp_server
+{
+    internal class AutosupportLanguageDefinition : IAutosupportLanguageDefinition
+    {
+        public AutosupportLanguageDefinition(string languageId, string languageFilePattern)
+        {
+            LanguageId = languageId;
+            LanguageFilePattern = languageFilePattern;
+        }
+
+        public string LanguageId { get; }
+        public string LanguageFilePattern  { get; }
+    }
+}
---
+++ autosupport-lsp-server/Program.cs
@@ -0,0 +1,30 @@
+﻿using OmniSharp.Extensions.LanguageServer.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace autosupport_lsp_server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SetupDocumentStore(args);
+
+            var server = await LanguageServer.From(options =>
+            {
+                options
+                    .WithInput(Console.OpenStandardInput())
+                    .WithOutput(Console.OpenStandardOutput())
+                    .WithHandler<TextDocumentSyncHandler>();
+            });
+
+            await server.WaitForExit;
+        }
+
+        static private void SetupDocumentStore(string[] args)
+        {
+            // TODO: instead have a path to the file that will be deserialized
+            DocumentStore.LanguageDefinition = new AutosupportLanguageDefinition(args[0], args[1]);
+        }
+    }
+}
