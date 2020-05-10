d320954a8cef0f37b80ebc3de0cc5a5b68a31533
Oscar Ablinger <dev.ablinger@gmail.com>
2020-05-10 21:20:36 +0200

    test(parsing): test for rule with 1 terminal

---
+++ Tests/Parsing/Impl/ParserTest.cs
@@ -0,0 +1,49 @@
+﻿using autosupport_lsp_server;
+using autosupport_lsp_server.Parsing.Impl;
+using autosupport_lsp_server.Symbols;
+using Moq;
+using System.Collections.Generic;
+using Tests.Terminals.Impl.Mocks;
+using Xunit;
+
+namespace Tests.Parsing.Impl
+{
+    public class ParserTest
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        void When_RuleWithOneTerminal_ThenParsesCorrectly(bool shouldParse)
+        {
+            // given
+            string parseString = "terminalString";
+
+            var terminal = new Mock<MockTerminal>
+            {
+                CallBase = true // for both Match functions & IsTerminal
+            };
+            terminal.SetupGet(t => t.MinimumNumberOfCharactersToParse).Returns(parseString.Length);
+
+            terminal.Setup(t => t.TryParse(It.IsAny<string>())).Returns(shouldParse);
+
+            var rule = new Mock<IRule>();
+            rule.SetupGet(r => r.MinimumNumberOfCharactersToParse).Returns(1);
+            rule.SetupGet(r => r.Symbols).Returns(new List<ISymbol>() { terminal.Object });
+
+            var languageDefinition = new Mock<IAutosupportLanguageDefinition>();
+            languageDefinition.SetupGet(el => el.StartRules).Returns(new string[] { "S" });
+            languageDefinition.SetupGet(el => el.Rules).Returns(new Dictionary<string, IRule>()
+            {
+                { "S", rule.Object }
+            });
+
+            // when
+            var result = Parser.Parse(
+                languageDefinition.Object,
+                Document.FromText("uri", "stringToken"));
+                
+            // then
+            Assert.Equal(shouldParse, result.FinishedSuccessfully);
+        }
+    }
+}
---
+++ Tests/Terminals/Impl/Mocks/MockTerminal.cs
@@ -0,0 +1,23 @@
+﻿using autosupport_lsp_server.Symbols;
+using autosupport_lsp_server.Symbols.Impl;
+using System;
+
+namespace Tests.Terminals.Impl.Mocks
+{
+    internal abstract class MockTerminal : Symbol, ITerminal
+    {
+        public abstract int MinimumNumberOfCharactersToParse { get; }
+
+        public abstract bool TryParse(string str);
+
+        public override void Match(Action<ITerminal> terminal, Action<INonTerminal> nonTerminal, Action<IAction> action, Action<IOperation> operation)
+        {
+            terminal.Invoke(this);
+        }
+
+        public override R Match<R>(Func<ITerminal, R> terminal, Func<INonTerminal, R> nonTerminal, Func<IAction, R> action, Func<IOperation, R> operation)
+        {
+            return terminal.Invoke(this);
+        }
+    }
+}
