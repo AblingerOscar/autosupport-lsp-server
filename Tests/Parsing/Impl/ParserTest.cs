04e73f68c930889ac5638eff65ac699bb607b9bf
Oscar Ablinger <dev.ablinger@gmail.com>
2020-05-11 17:26:05 +0200

    test(parsing): test for nonterminal

--- Tests/Parsing/Impl/ParserTest.cs
+++ Tests/Parsing/Impl/ParserTest.cs
@@ -27,7 +27,6 @@ namespace Tests.Parsing.Impl
             terminal.Setup(t => t.TryParse(It.IsAny<string>())).Returns(shouldParse);
 
             var rule = new Mock<IRule>();
-            rule.SetupGet(r => r.MinimumNumberOfCharactersToParse).Returns(1);
             rule.SetupGet(r => r.Symbols).Returns(new List<ISymbol>() { terminal.Object });
 
             var languageDefinition = new Mock<IAutosupportLanguageDefinition>();
@@ -44,6 +43,52 @@ namespace Tests.Parsing.Impl
 
             // then
             Assert.Equal(shouldParse, result.FinishedSuccessfully);
+        }
+
+        [Fact]
+        void When_RuleWithNonTerminal_ThenResolvesCorrectly()
+        {
+            // given
+            string referencedRuleName = "ReferencedRule";
+            string parseString = "terminalString";
+
+            var nonTerminal = new Mock<MockNonTerminal>
+            {
+                CallBase = true // for both Match functions & IsTerminal
+            };
+            nonTerminal.SetupGet(nt => nt.ReferencedRule).Returns(referencedRuleName);
+
+            var terminal = new Mock<MockTerminal>
+            {
+                CallBase = true // for both Match functions & IsTerminal
+            };
+            terminal.SetupGet(t => t.MinimumNumberOfCharactersToParse).Returns(parseString.Length);
+
+            terminal.Setup(t => t.TryParse(It.IsAny<string>())).Returns(true);
+
+            var baseRule = new Mock<IRule>();
+            baseRule.SetupGet(r => r.Symbols).Returns(new List<ISymbol>() { nonTerminal.Object });
+
+            var referencedRule = new Mock<IRule>();
+            referencedRule.SetupGet(r => r.Symbols).Returns(new List<ISymbol>() { terminal.Object });
+
+
+            var languageDefinition = new Mock<IAutosupportLanguageDefinition>();
+            languageDefinition.SetupGet(el => el.StartRules).Returns(new string[] { "S" });
+            languageDefinition.SetupGet(el => el.Rules).Returns(new Dictionary<string, IRule>()
+            {
+                { "S", baseRule.Object },
+                { referencedRuleName, referencedRule.Object }
+            });
+
+            // when
+            var result = Parser.Parse(
+                languageDefinition.Object,
+                Document.FromText("uri", "stringToken"));
+
+            // then
+            Assert.True(result.FinishedSuccessfully);
+            terminal.Verify(t => t.TryParse(It.IsAny<string>()), Times.Once());
         }
     }
 }
