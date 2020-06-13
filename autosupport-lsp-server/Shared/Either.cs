e46c72801ee55d250f1b2c309e96d9800bff0d6a
Oscar Ablinger <dev.ablinger@gmail.com>
2020-06-13 15:31:07 +0200

    feat: add comparison operators to Either

--- autosupport-lsp-server/Shared/Either.cs
+++ autosupport-lsp-server/Shared/Either.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace autosupport_lsp_server.Shared
 {
@@ -12,6 +13,18 @@ namespace autosupport_lsp_server.Shared
         public static implicit operator Either<TLeft, TRight>(TLeft left) => new Either<TLeft, TRight>(left);
 
         public static implicit operator Either<TLeft, TRight>(TRight right) => new Either<TLeft, TRight>(right);
+
+        public static bool operator== (Either<TLeft, TRight> either, TLeft other) => either.isLeft && either.left!.Equals(other);
+        public static bool operator!= (Either<TLeft, TRight> either, TLeft other) => !either.isLeft || !either.left!.Equals(other);
+
+        public static bool operator== (Either<TLeft, TRight> either, TRight other) => !either.isLeft && either.right!.Equals(other);
+        public static bool operator!= (Either<TLeft, TRight> either, TRight other) => either.isLeft || !either.right!.Equals(other);
+
+        public static bool operator== (TLeft other, Either<TLeft, TRight> either) => either.isLeft && either.left!.Equals(other);
+        public static bool operator!= (TLeft other, Either<TLeft, TRight> either) => !either.isLeft || !either.left!.Equals(other);
+
+        public static bool operator== (TRight other, Either<TLeft, TRight> either) => !either.isLeft && either.right!.Equals(other);
+        public static bool operator!= (TRight other, Either<TLeft, TRight> either) => either.isLeft || !either.right!.Equals(other);
 
 #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public Either(TLeft left)
@@ -36,5 +49,42 @@ namespace autosupport_lsp_server.Shared
         }
 
         public R Match<R>(Func<TLeft, R> leftFunc, Func<TRight, R> rightFunc) => isLeft ? leftFunc.Invoke(left) : rightFunc.Invoke(right);
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Either<TLeft, TRight> either &&
+                   isLeft == either.isLeft &&
+                   EqualityComparer<TLeft>.Default.Equals(left, either.left) &&
+                   EqualityComparer<TRight>.Default.Equals(right, either.right);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(isLeft, left, right);
+        }
+
+        public override string? ToString()
+        {
+            return Match(l => $"{l}", r => $"{r}");
+        }
+    }
+
+    public static class Either
+    {
+        public static Either<TLeft, TRight> If<TLeft, TRight>(bool condition, TLeft left, TRight right)
+        {
+            if (condition)
+                return left;
+            else
+                return right;
+        }
+
+        public static Either<TLeft, TRight> If<TLeft, TRight>(bool condition, Func<TLeft> left, Func<TRight> right)
+        {
+            if (condition)
+                return left.Invoke();
+            else
+                return right.Invoke();
+        }
     }
 }
