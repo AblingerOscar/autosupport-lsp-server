5d7d06b4800aa110cd259d27fb77375ad35058fd
Oscar Ablinger <dev.ablinger@gmail.com>
2020-06-18 15:37:54 +0200

    feat(parser): remove RuleState dependency in ActionParser

--- autosupport-lsp-server/Parsing/Impl/ActionParser.cs
+++ autosupport-lsp-server/Parsing/Impl/ActionParser.cs
@@ -13,20 +13,38 @@ namespace autosupport_lsp_server.Parsing.Impl
 {
     public class ActionParser
     {
-        private delegate IConcreteRuleStateBuilder SpecificPostActionParser(ParseState parseState, RuleState ruleState, IAction action, Position startOfMarkings);
+        internal readonly struct ParserInformation
+        {
+            public readonly Uri Uri;
+            public readonly Position Position;
+            public readonly Func<Position, string> GetTextUpToPosition;
 
-        internal static IConcreteRuleStateBuilder ParseAction(ParseState parseState, RuleState ruleState, IAction action)
+            public ParserInformation(Uri uri, Position position, Func<Position, string> getTextUpToPosition)
+            {
+                Uri = uri;
+                Position = position;
+                GetTextUpToPosition = getTextUpToPosition;
+            }
+
+            public static implicit operator ParserInformation(ParseState parseState)
+                => new ParserInformation(parseState.Uri, parseState.Position, start => parseState.GetTextBetweenPositions(start));
+        }
+
+
+        private delegate IConcreteRuleStateBuilder SpecificPostActionParser(ParserInformation parseInfo, RuleState ruleState, IAction action, Position startOfMarkings);
+
+        internal static IConcreteRuleStateBuilder ParseAction(ParserInformation parseInfo, RuleState ruleState, IAction action)
         {
             switch(action.GetBaseCommand())
             {
                 case IAction.IDENTIFIER:
-                    return ParsePostAction(parseState, ruleState, action, ParseIdentifierAction);
+                    return ParsePostAction(parseInfo, ruleState, action, ParseIdentifierAction);
                 case IAction.IDENTIFIER_TYPE:
                     if (action.GetArguments()[0] == IAction.IDENTIFIER_TYPE_ARG_SET)
                         // do immediate action
                         return ruleState.Clone().WithValue(RuleStateValueStoreKey.NextType, action.GetArguments()[1]);
 
-                    return ParsePostAction(parseState, ruleState, action, ParseIdentifierTypeAction);
+                    return ParsePostAction(parseInfo, ruleState, action, ParseIdentifierTypeAction);
                 case IAction.DECLARATION:
                     return ParseDeclaration(ruleState);
             }
@@ -35,29 +53,29 @@ namespace autosupport_lsp_server.Parsing.Impl
         }
 
         private static IConcreteRuleStateBuilder ParsePostAction(
-            ParseState parseState,
+            ParserInformation parseInfo,
             RuleState ruleState,
             IAction action,
             SpecificPostActionParser specificActionParser)
         {
             if (ruleState.Markers.TryGetValue(action.GetBaseCommand(), out var pos))
             {
-                var newRuleState = specificActionParser.Invoke(parseState, ruleState, action, pos);
+                var newRuleState = specificActionParser.Invoke(parseInfo, ruleState, action, pos);
                 return newRuleState.WithoutMarker(action.GetBaseCommand());
             }
             else
             {
-                return ruleState.Clone().WithMarker(action.GetBaseCommand(), parseState.Position);
+                return ruleState.Clone().WithMarker(action.GetBaseCommand(), parseInfo.Position);
             }
         }
 
-        private static IConcreteRuleStateBuilder ParseIdentifierAction(ParseState parseState, RuleState ruleState, IAction action, Position startOfMarkings)
+        private static IConcreteRuleStateBuilder ParseIdentifierAction(ParserInformation parseInfo, RuleState ruleState, IAction action, Position startOfMarkings)
         {
-            var textBetweenMarkers = parseState.GetTextBetweenPositions(startOfMarkings);
+            var textBetweenMarkers = parseInfo.GetTextUpToPosition(startOfMarkings);
 
             ruleState.ValueStore.TryGetValue(RuleStateValueStoreKey.NextType, out string? type);
 
-            var declaration = GetIdentifierDeclaration(parseState, ruleState, startOfMarkings);
+            var declaration = GetIdentifierDeclaration(parseInfo, ruleState, startOfMarkings);
 
             if (textBetweenMarkers.Trim() != "")
             {
@@ -69,7 +87,7 @@ namespace autosupport_lsp_server.Parsing.Impl
                     {
                         Name = textBetweenMarkers,
                         References = new List<Reference>() {
-                            new Reference(parseState.Uri, new Range(startOfMarkings, parseState.Position.Clone()))
+                            new Reference(parseInfo.Uri, new Range(startOfMarkings, parseInfo.Position.Clone()))
                         },
                         Type = Either.If(type != null, type!, Identifier.IdentifierType.Any),
                         Declaration = declaration
@@ -78,7 +96,7 @@ namespace autosupport_lsp_server.Parsing.Impl
                 else
                 {
                     identifier.References.Add(
-                        new Reference(parseState.Uri, new Range(startOfMarkings, parseState.Position.Clone())));
+                        new Reference(parseInfo.Uri, new Range(startOfMarkings, parseInfo.Position.Clone())));
 
                     if (declaration != null)
                         identifier.Declaration = declaration;
@@ -96,17 +114,17 @@ namespace autosupport_lsp_server.Parsing.Impl
             return nextRuleState;
         }
 
-        private static DeclarationReference? GetIdentifierDeclaration(ParseState parseState, RuleState ruleState, Position startOfMarkings)
+        private static DeclarationReference? GetIdentifierDeclaration(ParserInformation parseInfo, RuleState ruleState, Position startOfMarkings)
         {
             if (ruleState.ValueStore.ContainsKey(RuleStateValueStoreKey.IsDeclaration))
-                return new DeclarationReference(parseState.Uri, new Range(startOfMarkings, parseState.Position), null);
+                return new DeclarationReference(parseInfo.Uri, new Range(startOfMarkings, parseInfo.Position.Clone()), null);
 
             return null;
         }
 
-        private static IConcreteRuleStateBuilder ParseIdentifierTypeAction(ParseState parseState, RuleState ruleState, IAction action, Position startOfMarkings)
+        private static IConcreteRuleStateBuilder ParseIdentifierTypeAction(ParserInformation parseInfo, RuleState ruleState, IAction action, Position startOfMarkings)
         {
-            return ruleState.Clone().WithValue(RuleStateValueStoreKey.NextType, parseState.GetTextBetweenPositions(startOfMarkings));
+            return ruleState.Clone().WithValue(RuleStateValueStoreKey.NextType, parseInfo.GetTextUpToPosition(startOfMarkings));
         }
 
         private static IConcreteRuleStateBuilder ParseDeclaration(RuleState ruleState)
